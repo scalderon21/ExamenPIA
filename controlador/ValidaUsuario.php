e8131a3358cb0aeb8a1eaa335fa74e31c5e6320a
sebai <sebai@DESKTOP-J753RRL>
2017-07-03 01:00:01 -0400

    Cambios menores

--- controlador/ValidaUsuario.php
+++ controlador/ValidaUsuario.php
@@ -1,17 +1,12 @@
 <?php
 include '../librerias.php';
+
 $oUsu=new Usuario($_REQUEST["nombreUsuario"],$_REQUEST["password"]);
 
-//Verifica que el usuario agregado no este en la base de datos
 if ($oUsu->AgregarUsuario()) {
     echo "Usuario Agregado";
 } else {
     echo "<b>Usuario ya existente</b>";
 }
-/*
-if ($oUsu->VerificaLocal()) {
-    echo "Todo bien";
-} else {
-    echo "<b>Todo mal</b>";
-}
-*/
+
+
--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 
-
 <html>
     <head>
+        <script src="js/jquery-3.2.1.min.js" type="text/javascript"></script>
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
         <?php
-        include 'paginas/Inicio.php';
+        include 'Inicio.php';
         ?>
+
     </body>
 </html>
