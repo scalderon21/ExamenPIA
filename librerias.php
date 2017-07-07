f512a987ae39be0d4b76397a7160ff9159eb8ec2
sebai <sebai@DESKTOP-J753RRL>
2017-07-06 21:35:48 -0400

    Librerias agregadas

--- librerias.php
+++ librerias.php
@@ -2,3 +2,6 @@
 include 'constantes.php';
 include PATH.'/lib/Conexion.php';
 include PATH.'/lib/Usuario.php';
+include PATH.'/lib/Paciente.php';
+include PATH.'/lib/Medico.php';
+include PATH.'/lib/Atencion.php';
